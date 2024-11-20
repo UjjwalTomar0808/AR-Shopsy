d14717baa306dcad5c45f2b6cbf83c6f58648059
ujjwal <ujjwaltomar.0808@gmail.com>
2024-11-20 20:31:28 +0530

    make some changes and add some product

--- src/data/ProductItems.js
+++ src/data/ProductItems.js
@@ -10,6 +10,8 @@ import OfficeChair from "../assets/models/OfficeChair.glb";
 import OfficeChairUsdz from "../assets/models/OfficeChair.usdz";
 import pot from "../assets/models/pot.glb";
 import potUsdz from "../assets/models/pot.usdz";
+import cloths from "../assets/models/cloths.glb";
+import clothsUsdz from "../assets/models/cloths.usdz";
 
 const productItems = [
   {
@@ -155,6 +157,30 @@ const productItems = [
       },
     ]
   },
-  
+  {
+    id: 1,
+    name: "Cloth",
+    modelSrc: cloths,
+    iOSSrc: clothsUsdz,
+    category: "Cloth",
+    color: "Orange",
+    annotations: [
+      {
+        title: "best-fabric",
+        slot: "hotspot-1",
+        position: "0.011597651675006926m 0.5744572599492905m -0.1383899854988515m",
+        normal: "0.028332494851243895m 0.2137467602998606m 0.9764781575625839m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      }, {
+        title: "best-design",
+        slot: "hotspot-2",
+        position: "0.008754174027053235m 0.3513235856998005m 0.1658749505478343m",
+        normal: "-0.30988561688489596m 0.9507625837296717m -0.004627507703580716m",
+        orbit: "10.89188deg 119.9775deg 0.03543022m",
+        target: "-0.1053838m 0.01610652m 0.1076345m"
+      },
+    ]
+  },
 ];
 export default productItems;
